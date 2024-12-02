a85eb3ad0ee29f448ff336b490edaf374cb014dd
László Sepsi <lsepsi@diligent.com>
2024-12-02 15:45:45 +0100

    add englis transaltion

--- typescript/src/game.ts
+++ typescript/src/game.ts
@@ -1,4 +1,4 @@
-export class GameState{
+export class GameState {
     public players: Array<string> = [];
     public places: Array<number> = [];
     public purses: Array<number> = [];
@@ -10,39 +10,66 @@ export class GameState{
     public scienceQuestions: Array<string> = [];
     public sportsQuestions: Array<string> = [];
     public rockQuestions: Array<string> = [];
+    public language: string = "en";
+    public l: any = {}
+
+}
+
+const transaltions = {
+    en: {
+        popQuestion: "Pop Question",
+        scineceQuestion: "Science Question",
+        sportsQuestion: "Sports Question",
+        rockQuestion: "Rock Question",
+        wasAdded: "was added",
+        theyPlayerAreNumber: "They are player number",
+        isTheCurrentPlayer: "is the current player",
+        isGettingOutOfPenaltyBox: "is getting out of the penalty box",
+        theyHaveRolledA: "They have rolled a",
+        sNewLocationIs: "'s new location is",
+        theCategoryIs: "The category is",
+        isNotGettingOutOfPenaltyBox: "is not getting out of the penalty box",
+        questionWasIncorrectlyAnswered: 'Question was incorrectly answered',
+        wasSentToPenaltyBox: "was sent to the penalty box",
+       answerWasCorrect: 'Answer was correct!',
+       nowHas: "now has",
+      goldCoins: "Gold Coins",
+
+
+    },
+    de: {},
+    hu: {},
+    zh: {}
 }
 
 export class Game {
-    private state:GameState;    
+    private state: GameState;
 
-    constructor(state?:GameState) {
-        if(state){
+    constructor(state?: GameState) {
+
+        if (state) {
             this.state = state
         }
-        else{
+        else {
             this.state = new GameState();
         }
-
+        this.state.l = transaltions[this.state.language]
         for (let i = 0; i < 50; i++) {
-            this.state.popQuestions.push("Pop Question " + i);
-            this.state.scienceQuestions.push("Science Question " + i);
-            this.state.sportsQuestions.push("Sports Question " + i);
-            this.state.rockQuestions.push(this.createRockQuestion(i));
-          }
-    }
-
-    private createRockQuestion(index: number): string {
-        return "Rock Question " + index;
+            this.state.popQuestions.push(this.state.l.popQuestion + " " + i);
+            this.state.scienceQuestions.push(this.state.l.scineceQuestion + " " + i);
+            this.state.sportsQuestions.push(this.state.l.sportsQuestion + " " + i);
+            this.state.rockQuestions.push(this.state.l.rockQuestion + " " + i);
+        }
     }
 
     public add(name: string): boolean {
         this.state.players.push(name);
-        this.state.places[this.howManyPlayers()-1] = 0;
-        this.state.purses[this.howManyPlayers()-1] = 0;
+        this.state.places[this.howManyPlayers() - 1] = 0;
+        this.state.purses[this.howManyPlayers() - 1] = 0;
         this.state.inPenaltyBox[this.howManyPlayers()] = false;
 
-        console.log(name + " was added");
-        console.log("They are player number " + this.state.players.length);
+        console.log(name + " " + this.state.l.wasAdded);
+        console.log(this.state.l.theyPlayerAreNumber + " " + this.state.players.length);
 
         return true;
     }
@@ -52,36 +79,36 @@ export class Game {
     }
 
     public roll(roll: number) {
-        console.log(this.state.players[this.state.currentPlayer] + " is the current player");
-        console.log("They have rolled a " + roll);
-    
+        console.log(this.state.players[this.state.currentPlayer] + " " + this.state.l.isTheCurrentPlayer);
+        console.log(this.state.l.theyHaveRolledA + " " + roll);
+
         if (this.state.inPenaltyBox[this.state.currentPlayer]) {
-          if (roll % 2 != 0) {
-            this.state.isGettingOutOfPenaltyBox = true;
-    
-            console.log(this.state.players[this.state.currentPlayer] + " is getting out of the penalty box");
+            if (roll % 2 != 0) {
+                this.state.isGettingOutOfPenaltyBox = true;
+
+                console.log(this.state.players[this.state.currentPlayer] + " " + this.state.l.isGettingOutOfPenaltyBox);
+                this.state.places[this.state.currentPlayer] = this.state.places[this.state.currentPlayer] + roll;
+                if (this.state.places[this.state.currentPlayer] > 11) {
+                    this.state.places[this.state.currentPlayer] = this.state.places[this.state.currentPlayer] - 12;
+                }
+
+                console.log(this.state.players[this.state.currentPlayer] + this.state.l.sNewLocationIs + " " + this.state.places[this.state.currentPlayer]);
+                console.log(this.state.l.theCategoryIs + " " + this.currentCategory());
+                this.askQuestion();
+            } else {
+                console.log(this.state.players[this.state.currentPlayer] + " " + this.state.l.isNotGettingOutOfThePenalty);
+                this.state.isGettingOutOfPenaltyBox = false;
+            }
+        } else {
+
             this.state.places[this.state.currentPlayer] = this.state.places[this.state.currentPlayer] + roll;
             if (this.state.places[this.state.currentPlayer] > 11) {
-              this.state.places[this.state.currentPlayer] = this.state.places[this.state.currentPlayer] - 12;
+                this.state.places[this.state.currentPlayer] = this.state.places[this.state.currentPlayer] - 12;
             }
-    
-            console.log(this.state.players[this.state.currentPlayer] + "'s new location is " + this.state.places[this.state.currentPlayer]);
-            console.log("The category is " + this.currentCategory());
+
+            console.log(this.state.players[this.state.currentPlayer] + this.state.l.sNewLocationIs + " " + this.state.places[this.state.currentPlayer]);
+            console.log(this.state.l.theCategoryIs + " " + this.currentCategory());
             this.askQuestion();
-          } else {
-            console.log(this.state.players[this.state.currentPlayer] + " is not getting out of the penalty box");
-            this.state.isGettingOutOfPenaltyBox = false;
-          }
-        } else {
-    
-          this.state.places[this.state.currentPlayer] = this.state.places[this.state.currentPlayer] + roll;
-          if (this.state.places[this.state.currentPlayer] > 11) {
-            this.state.places[this.state.currentPlayer] = this.state.places[this.state.currentPlayer] - 12;
-          }
-    
-          console.log(this.state.players[this.state.currentPlayer] + "'s new location is " + this.state.places[this.state.currentPlayer]);
-          console.log("The category is " + this.currentCategory());
-          this.askQuestion();
         }
     }
 
@@ -123,10 +150,10 @@ export class Game {
     }
 
     public wrongAnswer(): boolean {
-        console.log('Question was incorrectly answered');
-        console.log(this.state.players[this.state.currentPlayer] + " was sent to the penalty box");
+        console.log(this.state.l.questionWasIncorrectlyAnswered);
+        console.log(this.state.players[this.state.currentPlayer] + " " + this.state.l.wasSentToPenaltyBox);
         this.state.inPenaltyBox[this.state.currentPlayer] = true;
-    
+
         this.state.currentPlayer += 1;
         if (this.state.currentPlayer == this.state.players.length)
             this.state.currentPlayer = 0;
@@ -136,41 +163,41 @@ export class Game {
     public wasCorrectlyAnswered(): boolean {
         if (this.state.inPenaltyBox[this.state.currentPlayer]) {
             if (this.state.isGettingOutOfPenaltyBox) {
-              console.log('Answer was correct!');
-              this.state.purses[this.state.currentPlayer] += 1;
-              console.log(this.state.players[this.state.currentPlayer] + " now has " +
-              this.state.purses[this.state.currentPlayer] + " Gold Coins.");
-      
-              var winner = this.didPlayerWin();
-              this.state.currentPlayer += 1;
-              if (this.state.currentPlayer == this.state.players.length)
-                this.state.currentPlayer = 0;
-      
-              return winner;
+                console.log(this.state.l.answerWasCorrect);
+                this.state.purses[this.state.currentPlayer] += 1;
+                console.log(this.state.players[this.state.currentPlayer] + " " + this.state.l.nowHas + " " +
+                    this.state.purses[this.state.currentPlayer] + " " + this.state.l.goldCoins);
+
+                var winner = this.didPlayerWin();
+                this.state.currentPlayer += 1;
+                if (this.state.currentPlayer == this.state.players.length)
+                    this.state.currentPlayer = 0;
+
+                return winner;
             } else {
-              this.state.currentPlayer += 1;
-              if (this.state.currentPlayer == this.state.players.length)
-                this.state.currentPlayer = 0;
-              return true;
+                this.state.currentPlayer += 1;
+                if (this.state.currentPlayer == this.state.players.length)
+                    this.state.currentPlayer = 0;
+                return true;
             }
-      
-      
-          } else {
-      
-            console.log("Answer was correct!");
-      
+
+
+        } else {
+
+            console.log(this.state.l.answerWasCorrect);
+
             this.state.purses[this.state.currentPlayer] += 1;
-            console.log(this.state.players[this.state.currentPlayer] + " now has " +
-                this.state.purses[this.state.currentPlayer] + " Gold Coins.");
-      
+            console.log(this.state.players[this.state.currentPlayer] + " " +  this.state.l.nowHas + " " +
+                this.state.purses[this.state.currentPlayer] + " " + this.state.l.goldCoins + ".");
+
             var winner = this.didPlayerWin();
-      
+
             this.state.currentPlayer += 1;
             if (this.state.currentPlayer == this.state.players.length)
                 this.state.currentPlayer = 0;
-      
+
             return winner;
-          }
+        }
     }
 
 }
