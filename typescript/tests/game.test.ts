b9f105d2cf6c9b5fcad081cdc29f1e2ea3ff0c87
László Sepsi <lsepsi@diligent.com>
2024-12-02 10:31:46 +0100

    Add console log mock

--- typescript/tests/game.test.ts
+++ typescript/tests/game.test.ts
@@ -1,8 +1,30 @@
 import {expect} from 'chai';
-import {describe, it} from 'mocha';
+import {describe, it, beforeEach, afterEach} from 'mocha';
 import {GameRunner} from '../src/game-runner';
+import { Game } from '../src/game';
+
+const originalConsoleLog = console.log;
+const logs:any[] = []
+let inTest = false
+
+// Override console.log
 
 describe('The test environment', () => {
+    beforeEach(()=>{
+        console.log = (...args: any[]) => {
+            if(inTest){
+                logs.push(args)
+            }
+            
+            originalConsoleLog("Overridden Log:", ...args);
+        };
+    })
+
+    afterEach(()=>{
+        inTest = false
+        console.log = originalConsoleLog
+    })
+
     it('should pass', () => {
         expect(true).to.be.true;
     });
@@ -11,4 +33,13 @@ describe('The test environment', () => {
         expect(GameRunner).to.not.be.undefined;
     });
 
+    it("should add player", function () {
+        inTest = true
+        const game = new Game();
+        game.add("Player")
+        const expected = ["Player was added","They are player number 1"]
+        expect(logs).to.deep.equal(expected);
+    });
 });
+
+
